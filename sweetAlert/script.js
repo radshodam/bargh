ef5e3315293fe1b01f20ef6ef51328b2662cb4ff
mohamadZareai <mohamadzareai@gmail.com>
2020-10-13 12:33:22 +0330

    creat repository barghi

---
+++ sweetAlert/script.js
@@ -0,0 +1,10 @@
+// function accept() {
+//     Swal.fire({
+//         position: 'top-center',
+//         icon: 'success',
+//         title: 'با موفقیت انجام شد',
+//         showConfirmButton: false,
+//         timer: 3000
+//     })
+//
+// };
